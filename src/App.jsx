591799203ae57938884b580081e0b0e094ceec45
Punkistein <saulleon64@hotmail.com>
2022-06-10 10:36:24 -0500

    Presentable materia Fundamentos

--- src/App.jsx
+++ src/App.jsx
@@ -48,61 +48,61 @@ function App() {
           <img className='principal--imagenFondo' ></img>
         </section>
         <section className='proyectos' id='proj'>
-          
-          <img className='projects--imagenFondo' ></img>
-          <h2 className = 'proyectos--titulo'>Proyectos </h2>
-          <div>
-          <Card
-          image={Pro1}
-          title='Desarrollo de Software'
-          description='Desarrollamos software'
-          />
 
-          <Card
-           image= {Pro2}
-          title='Talleres'
-          description='Realizamos Talleres'
-          />
-          </div>
-                    
+          <img className='projects--imagenFondo' ></img>
+          <h2 className='proyectos--titulo'>Proyectos </h2>
           <div>
-          <Card
-           image= {Pro3}
-          title='Capacitaciones'
-          description='Hacemos Capacitaciones '
-          />
-          
+            <Card
+              image={Pro1}
+              title='Desarrollo de Software'
+              description='Desarrollamos software'
+            />
+
+            <Card
+              image={Pro2}
+              title='Talleres'
+              description='Realizamos Talleres'
+            />
+          </div>
+
+          <div>
+            <Card
+              image={Pro3}
+              title='Capacitaciones'
+              description='Hacemos Capacitaciones '
+            />
+
           </div>
           <div>
-          <article>
-          <h2 >Contamos con proyectos novedosos que te ayudaran a comprender mejor los conocimientos de programacion</h2>
-          
-          </article>
+            <article>
+              <h2 >Contamos con proyectos novedosos que te ayudaran a comprender mejor los conocimientos de programacion</h2>
+
+            </article>
           </div>
-          
-        
+
+
         </section>
         <section className='quienesSomos' id='about'>
           <img className='about--imagenFondo' ></img>
           <img src={ImagenQs} className='imagenQs' />
           <img src={logoUTC} className='principal--logo utc' />
           <div className='sectionQs'>
-           <h1>Quienes somos?
-          </h1>
-          <div className="infoQs">
-          <p >
-              Somos estudiantes universitarios<br /><br />
+            <h1>Quienes somos?
+            </h1>
+            <div className="infoQs">
+              <p >
+                Somos estudiantes universitarios<br /><br />
 
-              Equipo de trabajo : <br /><br />
-              -Saul Guerrero <br />
-              -Kevin Loja <br />
-              -Ronald Rodriguez <br />
-              -Javier Lasso <br /><br />
+                Equipo de trabajo : <br /><br />
+                -Saul Guerrero <br />
+                -Kevin Loja <br />
+                -Ronald Rodriguez <br />
+                -Javier Lasso <br /><br />
 
-              Estudiantes del mismo semestre de ingenieria en sistemas de informacion . <br /><br />
-              Contamos con el apoyo de la direccion de carrera de la universidad Tecnica de Cotopaxi  de parte de nuestra coordinadora Veronica Tapia </p>
+                Estudiantes del mismo semestre de ingenieria en sistemas de informacion . <br /><br />
+                Contamos con el apoyo de la direccion de carrera de la universidad Tecnica de Cotopaxi  de parte de nuestra coordinadora Veronica Tapia </p>
+            </div>
           </div>
-              </div>
 
         </section>
         <section className='blog' id='blog'>
@@ -122,13 +122,13 @@ function App() {
             title='Herramientas tecnológicas estudiantiles'
             text='Lorem ipsum dolor sit amet, consectetur adipiscing elit.
             Donec euismod, nisl eget consectetur sagittis, nisl nunc'
-            image ={blogEdu}
+            image={blogEdu}
           />
           <BlogCard
             title='La UTC en 2022'
             text='Lorem ipsum dolor sit amet, consectetur adipiscing elit.
             Donec euismod, nisl eget consectetur sagittis, nisl nunc'
-            image ={blogUTC}
+            image={blogUTC}
           />
           <img className='blog--imagenFondo' ></img>
         </section>
